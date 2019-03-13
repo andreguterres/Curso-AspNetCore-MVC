183dbb8006510277ba9f3f641d2bc606394ccdfd
CATALOG <CATALOG@THINKPAD-E430CA>
2019-03-13 14:44:08 -0300

    Rotas customizadas, Rota trazendo statuscode, texto, imagens

--- AspNetCore-MVC/AspNetCore-MVC/Controllers/ProductController.cs
+++ AspNetCore-MVC/AspNetCore-MVC/Controllers/ProductController.cs
@@ -10,14 +10,53 @@ namespace AspNetCore_MVC.Controllers
     [Route("api/product")]
     public class ProductController : Controller
     {
-        //validação de da rota. pode escolher o tipo, mnº máximo e minimo de caracteres, etc.
-        [HttpGet("{id=int}")]
+        #region//validação de da rota. pode escolher o tipo, nº máximo e minimo de caracteres, etc.
 
-        public int Get(int id) {
+        //[HttpGet("{id=int}")]
 
-            return id;
+        //public int Get(int id) {
+
+        //    return id;
+        #endregion
+
+        #region// Imprimi na tela a patch "api/product".
+        //public string Get()
+        //{
+
+        //    // Imprimi na tela a patch "api/product".
+        //    return HttpContext.Request.Path;
+
+        //}
+        #endregion
+
+        #region// Parametro enviado por Query String
+        //public string Get()
+        //{
+
+        //    // Parametro enviado por Query String
+        //    // Exemplo de Url com a String Digitada
+        //    //https://localhost:5001/api/product?param= Olá Mundo !
+        //    return HttpContext.Request.Query["param"];
+
+        //}
+        #endregion
+
+
+        #region//Pode retornar uma página, um arquivo df, imagem, etc..
+        //public string Get()
+        public IActionResult Get()
+        {
+            //Pode customizar o tipo de retorno.
+            HttpContext.Response.StatusCode = 404;
+            HttpContext.Response.Headers.Add("ResponseHeader", "Ola Mundo !");
+
+            //return "ok";
+
+            //Retorna Imagem
+            return File("images/NewTux.svg", "image/svg+xml");
+
         }
-
+        #endregion
     }
 }
 
